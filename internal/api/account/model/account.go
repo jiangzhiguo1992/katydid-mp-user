034fae43ece03c8d4478e50db95b9f4fa8cc6f14
Jiang <jiangzhiguo1992@163.com>
2025-03-06 16:45:48 +0800

    add account

--- internal/api/account/model/account.go
+++ internal/api/account/model/account.go
@@ -2,145 +2,191 @@ package model
 
 import (
 	"katydid-mp-user/internal/pkg/model"
-	"time"
+	"katydid-mp-user/utils"
 )
 
-const (
-	AccountKindPwd   uint16 = 1 // 账号+密码
-	AccountKindPhone uint16 = 2 // 手机+短信码
-	AccountKindEmail uint16 = 3 // 邮箱+邮件码
-	AccountKindBio   uint16 = 4 // 生物特征
-	AccountKingThird uint16 = 5 // 第三方+平台验证
-)
-
-// Account 账户
+// Account 账号结构
 type Account struct {
 	*model.Base
 
-	UserId uint64 `json:"userId"` // 用户Id
+	UserID *uint64 `json:"userId"` // 账号拥有者Id (有些org/app不填user)
 
-	//AuthKinds []int             `json:"authKinds"` // 可用验证类型
-	//AuthOnAts map[int][]int64   `json:"authOnAts"` // [kind][]验证发送时间
-	//AuthBodys map[int]*AuthInfo `json:"authBodys"` // [kind]验证内容
-	//AuthOkAts map[int][]int64   `json:"authOkAts"` // [kind][]验证成功时间
+	Nickname string        `json:"nickname"` // 昵称 (没有user的app/org会用这个，放外面是方便搜索)
+	Status   AccountStatus `json:"status"`   // 账号状态
 
-	clientId int64 `json:"clientId"` // 客户端Id
-	orgId    int64 `json:"orgId"`    // 组织Id
-	//Logins   map[int64][]LoginInfo     `json:"logins"`   // [clientId]登录信息
+	OrgActiveAts map[uint64]int64  `json:"orgActiveAts"` // [org]激活组织时间集合 (最早的就是注册的平台)
+	OrgTokens    map[uint64]string `json:"orgTokens"`    // [org]token列表(jwt)
+	OrgExpireAts map[uint64]int64  `json:"orgExpireAts"` // [org]token过期时间列表 -1为永久
+	AppActiveAts map[uint64]int64  `json:"appActiveAts"` // [app]激活应用时间集合 (最早的就是注册的平台)
+	AppTokens    map[uint64]string `json:"appTokens"`    // [app]token列表(jwt)
+	AppExpireAts map[uint64]int64  `json:"appExpireAts"` // [app]token过期时间列表 -1为永久
 
-	ActiveAts   map[uint64][]int64  `json:"activeAts"`   // [clientId]激活/登录客户端时间集合 (最早的就是注册的平台)
-	Tokens      map[uint64][]string `json:"tokens"`      // [clientId]访问tokens
-	ToExpireAts map[uint64][]int64  `json:"toExpireAts"` // [clientId]访问tokens过期时间 -1为永久
-	Enables     map[uint64]bool     `json:"enables"`     // [clientId]是否可用 (-1为都不可用)
-	Reasons     map[uint64]string   `json:"reasons"`     // [clientId]拒绝原因 (-1为都不可用)
+	// 认证方式引用 TODO:GG 是否可认证和必须认证的方式，由org/client决定
+	AuthPassword     *UsernamePwd      `json:"authPassword,omitempty"`     // 用户名密码认证
+	AuthPhone        *PhoneNumber      `json:"authPhone,omitempty"`        // 手机号认证
+	AuthEmail        *EmailAddress     `json:"authEmail,omitempty"`        // 邮箱认证
+	AuthBiometrics   []*Biometric      `json:"authBiometrics,omitempty"`   // 生物特征认证列表
+	AuthThirdParties []*ThirdPartyLink `json:"authThirdParties,omitempty"` // 第三方认证列表
 
-	AuthKinds []int `json:"authKinds"` // 可用验证类型
-
-	//LastSigninTime int64
-	//LastSigninIp   int64
-	// TODO:GG 下面两个要不要精简上去
-	//LastLoginAt time.Time `json:"lastLoginAt"`
-	// Actives
-	//ClientIds uint64 `json:"clientIds"` // 客户端Id
-	//Logins   map[int64][]LoginInfo     `json:"logins"`   // [clientId]登录信息
-	//Versions map[int64][]ClientVersion `json:"versions"` // 版本分布
-}
-
-func NewAccountDef(userId uint64) *Account {
-	return &Account{
-		Base:   model.NewBaseEmpty(),
-		UserId: userId,
-		//AuthKinds: []int{},
-		//AuthOnAts: map[int][]int64{},
-		//AuthBodys: map[int]*AuthInfo{},
-		//AuthOkAts: map[int][]int64{},
-		//
-		//ActiveAts:   map[uint64][]int64{},
-		//Tokens:      map[uint64][]string{},
-		//ToExpireAts: map[uint64][]int64{},
-		//Enables:     map[uint64]bool{},
-		//Reasons:     map[uint64]string{},
-
-		//Extra: map[string]any{},
-
-		//Logins:   map[int64][]LoginInfo{},
-		//Versions: map[int64][]ClientVersion{},
-	}
+	LoginHistory []*LoginInfo `json:"loginHistory,omitempty"` // 登录历史
 }
 
 func NewAccountEmpty() *Account {
 	return &Account{
-		//*base.DBModel
-		//AuthKinds: []int{},
-		//AuthOnAts: map[int][]int64{},
-		//AuthBodys: map[int]*AuthInfo{},
-		//AuthOkAts: map[int][]int64{},
-		//
-		//ActiveAts:   map[uint64][]int64{},
-		//Tokens:      map[uint64][]string{},
-		//ToExpireAts: map[uint64][]int64{},
-		//Enables:     map[uint64]bool{},
-		//Reasons:     map[uint64]string{},
-
-		//Extra: map[string]any{},
-
-		//Logins:   map[int64][]LoginInfo{},
-		//Versions: map[int64][]ClientVersion{},
+		Base:             model.NewBaseEmpty(),
+		OrgTokens:        make(map[uint64]string),
+		OrgActiveAts:     make(map[uint64]int64),
+		OrgExpireAts:     make(map[uint64]int64),
+		AppTokens:        make(map[uint64]string),
+		AppActiveAts:     make(map[uint64]int64),
+		AppExpireAts:     make(map[uint64]int64),
+		AuthBiometrics:   make([]*Biometric, 0),
+		AuthThirdParties: make([]*ThirdPartyLink, 0),
 	}
 }
 
-func (a *Account) IsTokenOk(clientId uint64, token string) (bool, bool) {
-	isTokenOk := false
-	isTokenExpireOk := false
-	if tokens, toOk := a.Tokens[clientId]; toOk {
-		for toIndex, to := range tokens {
-			if to == token {
-				isTokenOk = true
-				if expires, exOk := a.ToExpireAts[clientId]; exOk {
-					if len(expires) > toIndex {
-						expire := expires[toIndex]
-						if (expire == -1) || (expire > time.Now().UnixMilli()) {
-							isTokenExpireOk = true
-						}
-					}
-				}
-				break
-			}
+func NewAccount(nickname string) *Account {
+	return &Account{
+		Base:             model.NewBase(make(utils.KSMap)),
+		Nickname:         nickname,
+		Status:           AccountStatusNormal,
+		OrgTokens:        make(map[uint64]string),
+		OrgActiveAts:     make(map[uint64]int64),
+		OrgExpireAts:     make(map[uint64]int64),
+		AppTokens:        make(map[uint64]string),
+		AppActiveAts:     make(map[uint64]int64),
+		AppExpireAts:     make(map[uint64]int64),
+		AuthPassword:     nil,
+		AuthPhone:        nil,
+		AuthEmail:        nil,
+		AuthBiometrics:   make([]*Biometric, 0),
+		AuthThirdParties: make([]*ThirdPartyLink, 0),
+	}
+}
+
+// AccountStatus 账号状态
+type AccountStatus int8
+
+const (
+	AccountStatusBanned = -2 // 封禁 (不能访问任何api)
+	AccountStatusLocked = -1 // 锁定 (不能登录)
+	AccountStatusNormal = 0  // 正常 (必须有附带一个认证才能创建)
+)
+
+// Lock 锁定账号
+func (a *Account) Lock() {
+	a.Status = AccountStatusLocked
+}
+
+// Unlock 解锁账号
+func (a *Account) Unlock() {
+	a.Status = AccountStatusNormal
+}
+
+// Ban 封禁账号
+func (a *Account) Ban() {
+	a.Status = AccountStatusBanned
+}
+
+// AddRole 添加角色
+func (a *Account) AddRole(role string) {
+	roles, ok := a.GetRoles()
+	if !ok {
+		roles = make([]string, 0)
+	}
+	// 检查角色是否已存在
+	for _, r := range roles {
+		if r == role {
+			return
 		}
 	}
-	return isTokenOk, isTokenExpireOk
+	roles = append(roles, role)
+	a.SetRoles(&roles)
 }
 
-func (a *Account) NewToken(clientId uint64, userTokenMax int) (string, int64) {
-	if _, ok := a.Tokens[clientId]; !ok {
-		a.Tokens[clientId] = []string{}
+// HasRole 检查是否拥有指定角色
+func (a *Account) HasRole(role string) bool {
+	roles, ok := a.GetRoles()
+	if !ok {
+		return false
 	}
-	if _, ok := a.ToExpireAts[clientId]; !ok {
-		a.ToExpireAts[clientId] = []int64{}
+	for _, r := range roles {
+		if r == role {
+			return true
+		}
 	}
-	if len(a.Tokens[clientId]) >= userTokenMax {
-		a.Tokens[clientId] = a.Tokens[clientId][1:]
-		a.ToExpireAts[clientId] = a.ToExpireAts[clientId][1:]
-	}
-	newToken := "随机生成"
-	newTokenExpireAt := time.Now().UnixMilli() + 3600
-	a.Tokens[clientId] = append(a.Tokens[clientId], "随机生成")
-	a.ToExpireAts[clientId] = append(a.ToExpireAts[clientId], newTokenExpireAt)
-	return newToken, newTokenExpireAt
+	return false
 }
 
-// SetAvatarUrl 头像
-func (a *Account) SetAvatarUrl(supportUrl *string) {
-	if (supportUrl != nil) && (len(*supportUrl) > 0) {
-		a.Extra["avatarUrl"] = *supportUrl
-	} else {
-		delete(a.Extra, "avatarUrl")
-	}
+// RandomToken 生成随机token的真实实现
+func RandomToken() string {
+	// 此处应实现真正的随机token生成逻辑
+	// 例如生成UUID、安全随机字符串等
+	return "真实随机Token"
 }
 
-func (a *Account) GetSupportUrl() string {
-	if a.Extra["avatarUrl"] == nil {
-		return ""
-	}
-	return a.Extra["avatarUrl"].(string)
+const (
+	accExtraKeyAvatarId     = "avatarId"      // 头像ID
+	accExtraKeyAvatarUrl    = "avatarUrl"     // 头像URL
+	accExtraKeyStatusAt     = "statusAt"      // 状态时间
+	accExtraKeyStatusReason = "statusReason"  // 状态原因
+	accExtraKeyRoles        = "roles"         // 角色列表 (默认只有org下的用户有)
+	accExtraBioCredential   = "bioCredential" // 生物特征凭据
+	accExtraTPAccessToken   = "tpAccessToken" // 第三方认证访问令牌
+)
+
+func (a *Account) SetAvatarId(avatarId *int64) {
+	a.Extra.SetInt64(accExtraKeyAvatarId, avatarId)
+}
+
+func (a *Account) GetAvatarId() (int64, bool) {
+	return a.Extra.GetInt64(accExtraKeyAvatarId)
+}
+
+func (a *Account) SetAvatarUrl(avatarUrl *string) {
+	a.Extra.SetString(accExtraKeyAvatarUrl, avatarUrl)
+}
+
+func (a *Account) GetAvatarUrl() (string, bool) {
+	return a.Extra.GetString(accExtraKeyAvatarUrl)
+}
+
+func (a *Account) SetStatusAt(statusAt *int64) {
+	a.Extra.SetInt64(accExtraKeyStatusAt, statusAt)
+}
+
+func (a *Account) GetStatusAt() (int64, bool) {
+	return a.Extra.GetInt64(accExtraKeyStatusAt)
+}
+
+func (a *Account) SetStatusReason(reason *string) {
+	a.Extra.SetString(accExtraKeyStatusReason, reason)
+}
+
+func (a *Account) GetStatusReason() (string, bool) {
+	return a.Extra.GetString(accExtraKeyStatusReason)
+}
+
+func (a *Account) SetRoles(roles *[]string) {
+	a.Extra.SetStringSlice(accExtraKeyRoles, roles)
+}
+
+func (a *Account) GetRoles() ([]string, bool) {
+	return a.Extra.GetStringSlice(accExtraKeyRoles)
+}
+
+func (a *Account) SetBioCredential(bioCredential *string) {
+	a.Extra.SetString(accExtraBioCredential, bioCredential)
+}
+
+func (a *Account) GetBioCredential() (string, bool) {
+	return a.Extra.GetString(accExtraBioCredential)
+}
+
+func (a *Account) SetTPAccessToken(tpAccessToken *string) {
+	a.Extra.SetString(accExtraTPAccessToken, tpAccessToken)
+}
+
+func (a *Account) GetTPAccessToken() (string, bool) {
+	return a.Extra.GetString(accExtraTPAccessToken)
 }
