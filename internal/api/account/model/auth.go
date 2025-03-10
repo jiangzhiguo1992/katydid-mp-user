b2e7e2dabfd6c3a57fbb18e6674f9d6e9dcdba23
Jiang <jiangzhiguo1992@163.com>
2025-03-10 14:01:52 +0800

    update auth

--- internal/api/account/model/auth.go
+++ internal/api/account/model/auth.go
@@ -1,31 +1,23 @@
 package model
 
 import (
+	"errors"
 	"katydid-mp-user/internal/pkg/model"
 )
 
-//var _ IAuth = (*UsernamePwd)(nil)
-//var _ IAuth = (*PhoneNumber)(nil)
-//var _ IAuth = (*EmailAddress)(nil)
-//var _ IAuth = (*Biometric)(nil)
-//var _ IAuth = (*ThirdPartyLink)(nil)
+var _ IAuth = (*AuthPassword)(nil)
+var _ IAuth = (*AuthPhone)(nil)
+var _ IAuth = (*AuthEmail)(nil)
+var _ IAuth = (*AuthBiometric)(nil)
+var _ IAuth = (*AuthThirdParty)(nil)
 
 type (
 	IAuth interface {
-		// IsEnabled 检查认证方式是否启用
-		IsEnabled() bool
-
-		// IsActive 检查认证方式是否已激活过
-		IsActive() bool
-
-		// GetAccountID 获取关联的账号ID
-		GetAccountID() uint64
-
-		// GetKind 获取认证类型
-		GetKind() AuthKind
-
-		// Verify 验证认证信息
-		Verify(credential interface{}) (bool, error)
+		IsEnabled() bool                             // 检查认证方式是否启用
+		IsActive() bool                              // 检查认证方式是否已激活过
+		GetAccountID() uint64                        // 获取关联的账号ID
+		GetKind() AuthKind                           // 获取认证类型
+		Verify(credential interface{}) (bool, error) // 验证认证信息
 	}
 
 	// Auth 可验证账号基础
@@ -36,13 +28,14 @@ type (
 		Kind AuthKind `json:"kind"` // 认证类型
 	}
 
+	// AuthKind 认证类型
 	AuthKind int16
 
 	// AuthPassword 用户名+密码
 	AuthPassword struct {
-		Auth
-		Username string `json:"Username"` // 用户名
+		*Auth
 
+		Username string `json:"Username"`                      // 用户名
 		Salt     string `json:"-"`                             // 密码盐
 		Password string `json:"-" gorm:"column:password_hash"` // 密码 (md5)
 	}
@@ -50,9 +43,9 @@ type (
 	// AuthPhone 手机号+短信
 	AuthPhone struct {
 		*Auth
+
 		AreaCode string `json:"areaCode"` // 手机区号
 		Number   string `json:"number"`   // 手机号
-
 		Operator string `json:"operator"` // 运营商
 	}
 
@@ -63,68 +56,69 @@ type (
 		Domain   string `json:"domain"`   // 域名
 	}
 
-	// Biometric 生物特征
-	Biometric struct {
+	// AuthBiometric 生物特征
+	AuthBiometric struct {
 		*Auth
-		Kind int16 `json:"kind"` // 平台 (eg:人脸/指纹/声纹/...)
 
+		Kind AuthBioKind `json:"kind"` // 生物特征类型 (eg:人脸/指纹/声纹/...)
 		// extra => credential
 	}
 
-	// ThirdPartyLink 第三方账号链接
-	ThirdPartyLink struct {
+	// AuthThirdParty 第三方账号链接
+	AuthThirdParty struct {
 		*Auth
-		Kind   int16  `json:"kind"`   // 平台 (eg:微信/QQ/...)
-		OpenId string `json:"openId"` // 第三方平台唯一标识
 
-		// extra => accessToken, linkId, credential
+		Provider AuthTPProvider `json:"provider" gorm:"index"` // 平台 (eg:微信/QQ/...)
+		OpenId   string         `json:"openId"`                // 第三方平台唯一标识
+		// extra => AccessToken,RefreshToken, TokenExpiredAt, linkId, credential, UserInfo
 	}
+
+	// AuthBioKind 生物特征类型
+	AuthBioKind int16
+
+	// AuthTPProvider 第三方平台类型
+	AuthTPProvider int16
 )
 
-//func NewAuth(accountId uint64) *Auth {
-//	return &Auth{
-//		Base:      model.NewBaseEmpty(),
-//		AccountId: accountId,
-//		Enable:    true,
-//		IsActive:  false,
-//	}
-//}
-//
-//func NewUsernamePwdDef(accountId uint64, username, pwd string) *UsernamePwd {
-//	return &UsernamePwd{
-//		Auth:     NewAuthDef(accountId),
-//		Username: username,
-//		Password: pwd,
-//	}
-//}
-//
-//func NewPhoneNumberDef(accountId uint64, areaCode string, number string) *PhoneNumber {
-//	return &PhoneNumber{
-//		Auth:     NewAuthDef(accountId),
-//		AreaCode: areaCode,
-//		Number:   number,
-//		//Operator: operator,
-//	}
-//}
+// NewAuth 创建指定类型的认证实例
+func NewAuth(kind AuthKind, accountID uint64) IAuth {
+	auth := &Auth{
+		AccountID: accountID,
+		Kind:      kind,
+	}
+	switch kind {
+	case AuthKindPassword:
+		return &AuthPassword{Auth: auth}
+	case AuthKindPhone:
+		return &AuthPhone{Auth: auth}
+	case AuthKindEmail:
+		return &AuthEmail{Auth: auth}
+	case AuthKindBiometric:
+		return &AuthBiometric{Auth: auth}
+	case AuthKindThirdParty:
+		return &AuthThirdParty{Auth: auth}
+	default:
+		return nil
+	}
+}
 
 const (
 	AuthStatusBlock  model.Status = -1 // 封禁状态
 	AuthStatusInit   model.Status = 0  // 初始状态
 	AuthStatusActive model.Status = 1  // 激活状态
 
-	AuthKindUserPwd    AuthKind = 1 // 密码
-	AuthKindPhoneCode  AuthKind = 2 // 短信
-	AuthKindEmailCode  AuthKind = 3 // 邮箱
+	AuthKindPassword   AuthKind = 1 // 密码
+	AuthKindPhone      AuthKind = 2 // 短信
+	AuthKindEmail      AuthKind = 3 // 邮箱
 	AuthKindBiometric  AuthKind = 4 // 生物特征
 	AuthKindThirdParty AuthKind = 5 // 第三方平台
 
-	//AuthBioKindFace   int16 = 1 // 人脸
-	//AuthBioKindFinger int16 = 2 // 指纹
-	//AuthBioKindVoice  int16 = 3 // 声纹
-	//
-	//AuthThirdKindWechat int16 = 1 // 微信
-	//AuthThirdKindQQ     int16 = 2 // QQ
+	AuthBioKindFace   AuthBioKind = 1 // 人脸
+	AuthBioKindFinger AuthBioKind = 2 // 指纹
+	AuthBioKindVoice  AuthBioKind = 3 // 声纹
 
+	AuthThirdKindWechat AuthTPProvider = 1 // 微信
+	AuthThirdKindQQ     AuthTPProvider = 2 // QQ
 )
 
 func (a *Auth) IsEnabled() bool {
@@ -143,8 +137,41 @@ func (a *Auth) GetKind() AuthKind {
 	return a.Kind
 }
 
-func (a *Auth) Verify(credential interface{}) (bool, error) {
-	// TODO:GG
+func (a *Auth) Verify(_ interface{}) (bool, error) {
+	return false, errors.New("verify not implemented")
+}
+
+// Verify 实现密码验证
+func (p *AuthPassword) Verify(credential interface{}) (bool, error) {
+	cred, ok := credential.(struct {
+		Username string
+		Password string
+	})
+
+	if !ok {
+		return false, errors.New("invalid credential format")
+	}
+
+	// TODO:GG 实际场景中应该使用安全的密码哈希比较
+	if p.Username == cred.Username && p.Password == cred.Password {
+		return true, nil
+	}
+	return false, nil
+}
+
+// Verify 实现手机号验证
+func (p *AuthPhone) Verify(credential interface{}) (bool, error) {
+	cred, ok := credential.(struct {
+		AreaCode string
+		Number   string
+	})
+
+	if !ok {
+		return false, errors.New("invalid credential format")
+	}
+	if p.AreaCode == cred.AreaCode && p.Number == cred.Number {
+		return true, nil
+	}
 	return false, nil
 }
 
