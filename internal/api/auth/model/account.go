97c3982a7fc0a7457bce2e123012dd6d54df808e
Jiang <jiangzhiguo1992@163.com>
2025-03-14 22:15:33 +0800

    update account

--- internal/api/auth/model/account.go
+++ internal/api/auth/model/account.go
@@ -1,48 +1,54 @@
 package model
 
 import (
-	"errors"
 	"katydid-mp-user/internal/pkg/model"
 	"katydid-mp-user/pkg/data"
 	"time"
 )
 
-// Account 账号结构
-type Account struct {
-	*model.Base
-	UserID   *uint64 `json:"userId"`   // 账号拥有者Id (有些org/app不填user)
-	Nickname string  `json:"nickname"` // 昵称 (没有user的app/org会用这个，放外面是方便搜索)
+type (
+	// Account 账号结构
+	Account struct {
+		*model.Base
+		OwnKind  OwnKind `json:"ownKind"`  // 账号拥有者类型(注册的)
+		OwnID    uint64  `json:"ownId"`    // 账号拥有者ID(注册的)
+		UserID   *uint64 `json:"userId"`   // 账号拥有者Id (有些org/app不填user)
+		Nickname string  `json:"nickname"` // 昵称 (没有user的app/org会用这个，放外面是方便搜索)
 
-	Tokens    map[TokenOwn]map[uint64]string `json:"tokens"`    // [ownID] -> token列表(jwt)
-	ActiveAts map[TokenOwn]map[uint64]int64  `json:"activeAts"` // [ownID] -> 激活组织时间集合 (最早的就是注册的平台)
-	ExpireAts map[TokenOwn]map[uint64]int64  `json:"expireAts"` // [ownID] -> token过期时间列表 -1为永久 TODO:GG 不信任的设备，登录会RefreshToken
+		Tokens                map[OwnKind]map[uint64]string `json:"tokens"`        // [OwnKind][OwnID] -> token列表(jwt)
+		TokenExpireAts        map[OwnKind]map[uint64]int64  `json:"expireAts"`     // [OwnKind][OwnID] -> token过期时间列表 -1为永久
+		RefreshTokens         map[OwnKind]map[uint64]string `json:"refreshTokens"` // [OwnKind][OwnID] -> refreshToken列表(jwt)
+		RefreshTokenExpireAts map[OwnKind]map[uint64]int64  `json:"refExpireAts"`  // [OwnKind][OwnID] -> refreshToken过期时间列表 -1为永久
 
-	Auths map[AuthKind][]IAuth `json:"auths"` // 认证方式列表
-
-	//LoginHistory  []*Entry `json:"loginHistory"`  // 登录历史(login)
-	//EntryHistory  []any    `json:"entryHistory"`  // 进入历史(entry)
-	//AccessHistory []any    `json:"accessHistory"` // 访问历史(api)
-}
+		Auths map[AuthKind][]IAuth `json:"auths,omitempty"` // 认证方式列表 TODO:GG 没有auths了之后，就是unActive了
+		//LoginHistory  []*Entry `json:"loginHistory"`  // 登录历史(login)
+		//EntryHistory  []any    `json:"entryHistory"`  // 进入历史(entry)
+		//AccessHistory []any    `json:"accessHistory"` // 访问历史(api)
+	}
+)
 
 func NewAccountEmpty() *Account {
 	return &Account{
-		Base:      model.NewBaseEmpty(),
-		Tokens:    make(map[TokenOwn]map[uint64]string),
-		ActiveAts: make(map[TokenOwn]map[uint64]int64),
-		ExpireAts: make(map[TokenOwn]map[uint64]int64),
-		Auths:     make(map[AuthKind][]IAuth),
+		Base:                  model.NewBaseEmpty(),
+		Tokens:                make(map[OwnKind]map[uint64]string),
+		TokenExpireAts:        make(map[OwnKind]map[uint64]int64),
+		RefreshTokens:         make(map[OwnKind]map[uint64]string),
+		RefreshTokenExpireAts: make(map[OwnKind]map[uint64]int64),
+		Auths:                 make(map[AuthKind][]IAuth),
 	}
 }
 
-func NewAccount(userID *uint64, nickname string) *Account {
+func NewAccount(
+	ownKind OwnKind, ownID uint64, userID *uint64, nickname string,
+) *Account {
 	return &Account{
-		Base:      model.NewBase(make(data.KSMap)),
-		UserID:    userID,
-		Nickname:  nickname,
-		Tokens:    make(map[TokenOwn]map[uint64]string),
-		ActiveAts: make(map[TokenOwn]map[uint64]int64),
-		ExpireAts: make(map[TokenOwn]map[uint64]int64),
-		Auths:     make(map[AuthKind][]IAuth),
+		Base:    model.NewBase(make(data.KSMap)),
+		OwnKind: ownKind, OwnID: ownID, UserID: userID, Nickname: nickname,
+		Tokens:                make(map[OwnKind]map[uint64]string),
+		TokenExpireAts:        make(map[OwnKind]map[uint64]int64),
+		RefreshTokens:         make(map[OwnKind]map[uint64]string),
+		RefreshTokenExpireAts: make(map[OwnKind]map[uint64]int64),
+		Auths:                 make(map[AuthKind][]IAuth),
 	}
 }
 
@@ -63,37 +69,60 @@ func (a *Account) CanAccess() bool {
 	return a.Status >= AccountStatusActive
 }
 
-// GetAvailableAuthMethods 获取可用的认证方式列表
-func (a *Account) GetAvailableAuthMethods() []AuthKind {
-	methods := make([]AuthKind, 0)
+// GetAvailableAuthKinds 获取可用的认证方式列表
+func (a *Account) GetAvailableAuthKinds() []AuthKind {
+	kinds := make([]AuthKind, 0)
 	for kind, auths := range a.Auths {
 		if len(auths) > 0 {
-			methods = append(methods, kind)
+			for _, auth := range auths {
+				if auth != nil {
+					kinds = append(kinds, kind)
+					break
+				}
+			}
 		}
 	}
-	return methods
+	return kinds
 }
 
 // InvalidateToken 使token失效
-func (a *Account) InvalidateToken(ownKind TokenOwn, ownID uint64) {
+func (a *Account) InvalidateToken(ownKind OwnKind, ownID uint64) {
 	if owns, ok := a.Tokens[ownKind]; ok {
 		delete(owns, ownID)
 	}
-	if owns, ok := a.ExpireAts[ownKind]; ok {
+	if owns, ok := a.TokenExpireAts[ownKind]; ok {
+		delete(owns, ownID)
+	}
+	if owns, ok := a.RefreshTokens[ownKind]; ok {
+		delete(owns, ownID)
+	}
+	if owns, ok := a.RefreshTokenExpireAts[ownKind]; ok {
 		delete(owns, ownID)
 	}
 }
 
 // InvalidateAllTokens 使所有token失效
 func (a *Account) InvalidateAllTokens() {
-	a.Tokens = make(map[TokenOwn]map[uint64]string)
-	a.ExpireAts = make(map[TokenOwn]map[uint64]int64)
+	a.Tokens = make(map[OwnKind]map[uint64]string)
+	a.TokenExpireAts = make(map[OwnKind]map[uint64]int64)
+	a.RefreshTokens = make(map[OwnKind]map[uint64]string)
+	a.RefreshTokenExpireAts = make(map[OwnKind]map[uint64]int64)
 }
 
 // GetToken 获取token
-func (a *Account) GetToken(ownKind TokenOwn, ownID uint64) (string, bool) {
+func (a *Account) GetToken(ownKind OwnKind, ownID uint64) (string, bool) {
 	if owns, ok := a.Tokens[ownKind]; ok {
-		if token, ok := owns[ownID]; ok {
+		if token, okk := owns[ownID]; okk {
+			return token, true
+		}
+	}
+	return "", false
+}
+
+// GetRefreshToken 获取refreshToken
+func (a *Account) GetRefreshToken(ownKind OwnKind, ownID uint64) (string, bool) {
+	if owns, ok := a.RefreshTokens[ownKind]; ok {
+		if token, okk := owns[ownID]; okk {
 			return token, true
 		}
 	}
@@ -101,8 +130,21 @@ func (a *Account) GetToken(ownKind TokenOwn, ownID uint64) (string, bool) {
 }
 
 // IsTokenExpired 检查token是否已过期
-func (a *Account) IsTokenExpired(ownKind TokenOwn, ownID uint64) bool {
-	if expireAts, ok := a.ExpireAts[ownKind]; ok {
+func (a *Account) IsTokenExpired(ownKind OwnKind, ownID uint64) bool {
+	if expireAts, ok := a.TokenExpireAts[ownKind]; ok {
+		if expireAt, ok := expireAts[ownID]; ok {
+			if expireAt < 0 { // -1表示永不过期
+				return false
+			}
+			return time.Now().Unix() > expireAt
+		}
+	}
+	return true // 如果找不到过期时间，视为已过期
+}
+
+// IsRefreshTokenExpired 检查refreshToken是否已过期
+func (a *Account) IsRefreshTokenExpired(ownKind OwnKind, ownID uint64) bool {
+	if expireAts, ok := a.RefreshTokenExpireAts[ownKind]; ok {
 		if expireAt, ok := expireAts[ownID]; ok {
 			if expireAt < 0 { // -1表示永不过期
 				return false
@@ -115,67 +157,60 @@ func (a *Account) IsTokenExpired(ownKind TokenOwn, ownID uint64) bool {
 
 // GenerateTokens 为账号生成token
 func (a *Account) GenerateTokens(
-	ownKind TokenOwn, ownID uint64, jwtSecret string,
-	expireSec, refExpireHou int64, issuer string,
-) (*Token, error) {
-	// 检查账号状态
+	ownKind OwnKind, ownID uint64, issuer string,
+	jwtSecret string, expireSec, refExpireHou int64,
+) (*Token, bool) {
+	// 检查账号状态 TODO:GG 移到service?
 	if !a.CanLogin() {
-		return nil, errors.New("account cannot login")
+		return nil, false
 	}
 	// 旧的token
 	oldToken, _ := a.GetToken(ownKind, ownID)
 	// 创建新的Token
-	token := NewToken(a.ID, a.UserID, ownKind, ownID, expireSec, refExpireHou, issuer)
+	token := NewToken(ownKind, ownID, a.ID, a.UserID, issuer, expireSec, refExpireHou)
 	// 生成JWT令牌
 	if err := token.GenerateJWTTokens(jwtSecret, &oldToken); err != nil {
-		return nil, err
+		return nil, false
 	}
+	// 记录token
 	if a.Tokens[ownKind] == nil {
 		a.Tokens[ownKind] = make(map[uint64]string)
 	}
-	if a.ActiveAts[ownKind] == nil {
-		a.ActiveAts[ownKind] = make(map[uint64]int64)
+	if a.TokenExpireAts[ownKind] == nil {
+		a.TokenExpireAts[ownKind] = make(map[uint64]int64)
 	}
-	if a.ExpireAts[ownKind] == nil {
-		a.ExpireAts[ownKind] = make(map[uint64]int64)
-	}
-	// 记录token
 	a.Tokens[ownKind][ownID] = token.AccessToken
-	a.ExpireAts[ownKind][ownID] = time.Now().Add(time.Duration(token.ExpireSec) * time.Second).Unix()
-	// 如果是首次激活，记录激活时间
-	if _, exists := a.ActiveAts[ownKind][ownID]; !exists {
-		a.ActiveAts[ownKind][ownID] = time.Now().Unix()
-	}
-	return token, nil
+	a.TokenExpireAts[ownKind][ownID] = time.Now().Add(time.Duration(token.ExpireSec) * time.Second).Unix()
+	a.RefreshTokens[ownKind][ownID] = token.RefreshToken
+	a.RefreshTokenExpireAts[ownKind][ownID] = time.Now().Add(time.Duration(token.RefExpireHou) * time.Hour).Unix()
+	return token, true
 }
 
-// ValidateToken 验证token
+// ValidateToken 验证token/refreshToken
 func (a *Account) ValidateToken(
-	tokenStr string, ownKind TokenOwn, ownID uint64,
+	ownKind OwnKind, ownID uint64, token string,
 	jwtSecret string, checkExpire bool,
-) (*TokenClaims, error) {
-	// 检查账号状态
+) (*TokenClaims, bool) {
+	// 检查账号状态 TODO:GG 移到service?
 	if !a.CanAccess() {
-		return nil, errors.New("account cannot access")
+		return nil, false
 	}
 
-	// 验证token是否存在于账户中
+	// 验证token是否存在于账户中，是否匹配
 	storedToken, exists := a.GetToken(ownKind, ownID)
 	if !exists {
-		return nil, errors.New("token not found")
-	}
-
-	// 检查提供的token与存储的token是否匹配
-	if storedToken != tokenStr {
-		return nil, errors.New("invalid token")
+		return nil, false
+	} else if storedToken != token {
+		return nil, false
 	}
 
 	// 检查过期时间
 	if checkExpire && a.IsTokenExpired(ownKind, ownID) {
-		return nil, errors.New("token expired")
+		return nil, false
 	}
 	// 解析和验证JWT
-	return ParseJWT(tokenStr, jwtSecret, checkExpire)
+	claims, err := ParseJWT(token, jwtSecret, checkExpire)
+	return claims, err != nil
 }
 
 // AddRole 添加角色
